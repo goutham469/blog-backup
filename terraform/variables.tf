5ca2cce06395162698dc28251b79b1617cdb6560
Goutham Reddy UPPUNURI - I <g.uppunuri@f5.com>
2026-04-14 10:29:31 +0530

    requirements file added for python libraries, env variables updated on github actions, yaml file modified according to the error's role earlier

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,10 +8,12 @@ variable "project_name" {
   default = "blog-backup"
 }
 
+# when you change the bucket name here, make sure to update the bucket name in the lambda function code as well, which is passed as an environment variable from the github actions
 variable "s3_bucket_name" {
   default = "goutham469-blog-backup"
 }
 
+# if you want to change the lambda function name, you can do it here and remember to update the function name in the .yml file as well
 variable "lambda_function_name" {
   default = "blog-backup-lambda"
 }
